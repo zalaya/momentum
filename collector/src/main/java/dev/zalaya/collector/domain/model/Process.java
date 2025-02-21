603b2c4a2577e960e67f2fa3986abb46697c1e4b
Zalaya <enol.corzal@gmail.com>
2025-02-21 18:09:18 +0100

    feat: add memory usage to the process class

--- collector/src/main/java/dev/zalaya/collector/domain/model/Process.java
+++ collector/src/main/java/dev/zalaya/collector/domain/model/Process.java
@@ -4,12 +4,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@EqualsAndHashCode
+@EqualsAndHashCode(exclude = "memory")
 @Getter
 @RequiredArgsConstructor
 public class Process {
 
     private final String name;
     private final String path;
+    private final Long memory;
 
 }
--- collector/src/test/java/dev/zalaya/collector/domain/model/ProcessTest.java
+++ collector/src/test/java/dev/zalaya/collector/domain/model/ProcessTest.java
@@ -9,7 +9,7 @@ class ProcessTest {
     @Test
     void constructor_shouldCreateInstance() {
         // Act
-        Process process = new Process("process", "path/to/process");
+        Process process = new Process("process", "path/to/process", 100L);
 
         // Assert
         assertInstanceOf(Process.class, process);
@@ -19,7 +19,7 @@ class ProcessTest {
     void getName_shouldReturnName() {
         // Arrange
         String name = "process";
-        Process process = new Process(name, "path/to/process");
+        Process process = new Process(name, "path/to/process", 100L);
 
         // Assert
         assertEquals(name, process.getName());
@@ -29,10 +29,20 @@ class ProcessTest {
     void getPath_shouldReturnPath() {
         // Arrange
         String path = "path/to/process";
-        Process process = new Process("process", path);
+        Process process = new Process("process", path, 100L);
 
         // Assert
         assertEquals(path, process.getPath());
+    }
+
+    @Test
+    void getMemory_shouldReturnMemory() {
+        // Arrange
+        Long memory = 100L;
+        Process process = new Process("process", "path/to/process", memory);
+
+        // Assert
+        assertEquals(memory, process.getMemory());
     }
 
     @Test
@@ -40,8 +50,9 @@ class ProcessTest {
         // Arrange
         String name = "process";
         String path = "path/to/process";
-        Process process1 = new Process(name, path);
-        Process process2 = new Process(name, path);
+        Long memory = 100L;
+        Process process1 = new Process(name, path, memory);
+        Process process2 = new Process(name, path, memory);
 
         // Assert
         assertEquals(process1, process2);
@@ -50,8 +61,8 @@ class ProcessTest {
     @Test
     void equals_shouldReturnFalseWhenComparingTwoDifferentInstances() {
         // Arrange
-        Process process1 = new Process("process1", "path/to/process1");
-        Process process2 = new Process("process2", "path/to/process2");
+        Process process1 = new Process("process1", "path/to/process1", 100L);
+        Process process2 = new Process("process2", "path/to/process2", 100L);
 
         // Assert
         assertNotEquals(process1, process2);
