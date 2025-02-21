a12cd4c4be4f89effdf3fe7bb8d80486885568ec
Zalaya <enol.corzal@gmail.com>
2025-02-21 22:40:03 +0100

    test: add more tests for the new functionalities

--- monitor/src/main/java/dev/zalaya/collector/infrastructure/repository/ProcessJpaRepository.java
+++ monitor/src/main/java/dev/zalaya/collector/infrastructure/repository/ProcessJpaRepository.java
@@ -4,6 +4,15 @@ import dev.zalaya.collector.infrastructure.entity.ProcessEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProcessJpaRepository extends JpaRepository<ProcessEntity, Long> {
+
+    Optional<ProcessEntity> findByName(String name);
+    Optional<ProcessEntity> findByPath(String path);
+    List<ProcessEntity> findByMemoryLessThan(Long memory);
+    List<ProcessEntity> findByMemoryGreaterThan(Long memory);
+    List<ProcessEntity> findByMemoryBetween(Long minimumMemory, Long maximumMemory);
 
 }
--- monitor/src/test/java/dev/zalaya/collector/infrastructure/repository/ProcessJpaRepositoryTest.java
+++ monitor/src/test/java/dev/zalaya/collector/infrastructure/repository/ProcessJpaRepositoryTest.java
@@ -71,4 +71,87 @@ class ProcessJpaRepositoryTest {
         assertEquals(expectedProcess, foundProcess.get());
     }
 
+    @Test
+    @Sql(statements = {
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (1, 'process', 'path/to/process', 100);"
+    })
+    void findByName_shouldFindProcessByName() {
+        // Assure
+        String name = "process";
+        ProcessEntity expectedProcess = new ProcessEntity(1L, name, "path/to/process", 100L);
+
+        // Act
+        Optional<ProcessEntity> foundProcess = repository.findByName(name);
+
+        // Assert
+        assertEquals(expectedProcess, foundProcess.get());
+    }
+
+    @Test
+    @Sql(statements = {
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (1, 'process', 'path/to/process', 100);"
+    })
+    void findByPath_shouldFindProcessByPath() {
+        // Assure
+        String path = "path/to/process";
+        ProcessEntity expectedProcess = new ProcessEntity(1L, "process", path, 100L);
+
+        // Act
+        Optional<ProcessEntity> foundProcess = repository.findByPath(path);
+
+        // Assert
+        assertEquals(expectedProcess, foundProcess.get());
+    }
+
+    @Test
+    @Sql(statements = {
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (1, 'process1', 'path/to/process1', 100);",
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (2, 'process2', 'path/to/process2', 200);",
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (3, 'process3', 'path/to/process3', 300);"
+    })
+    void findByMemoryLessThan_shouldFindProcessesWithLessMemoryThan() {
+        // Assure
+        ProcessEntity expectedProcess = new ProcessEntity(1L, "process1", "path/to/process1", 100L);
+
+        // Act
+        List<ProcessEntity> foundProcess = repository.findByMemoryLessThan(200L);
+
+        // Assert
+        assertEquals(expectedProcess, foundProcess.get(0));
+    }
+
+    @Test
+    @Sql(statements = {
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (1, 'process1', 'path/to/process1', 100);",
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (2, 'process2', 'path/to/process2', 200);",
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (3, 'process3', 'path/to/process3', 300);"
+    })
+    void findByMemoryGreaterThan_shouldFindProcessesWithGreaterMemoryThan() {
+        // Assure
+        ProcessEntity expectedProcess = new ProcessEntity(3L, "process3", "path/to/process3", 300L);
+
+        // Act
+        List<ProcessEntity> foundProcess = repository.findByMemoryGreaterThan(200L);
+
+        // Assert
+        assertEquals(expectedProcess, foundProcess.get(0));
+    }
+
+    @Test
+    @Sql(statements = {
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (1, 'process1', 'path/to/process1', 100);",
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (2, 'process2', 'path/to/process2', 200);",
+        "INSERT INTO momentum.processes (id, name, path, memory) VALUES (3, 'process3', 'path/to/process3', 300);"
+    })
+    void findByMemoryBetween_shouldFindProcessesWithMemoryBetween() {
+        // Assure
+        ProcessEntity expectedProcess = new ProcessEntity(2L, "process2", "path/to/process2", 200L);
+
+        // Act
+        List<ProcessEntity> foundProcess = repository.findByMemoryBetween(150L, 250L);
+
+        // Assert
+        assertEquals(expectedProcess, foundProcess.get(0));
+    }
+
 }
