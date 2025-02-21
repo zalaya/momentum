ba412255dc42413f60815bb4dece9122920b9b41
Zalaya <enol.corzal@gmail.com>
2025-02-21 17:30:27 +0100

    refactor: remove the equals and hashcode from the process

--- collector/src/main/java/dev/zalaya/collector/domain/model/Process.java
+++ collector/src/main/java/dev/zalaya/collector/domain/model/Process.java
@@ -1,37 +1,13 @@
 package dev.zalaya.collector.domain.model;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public class Process {
 
     private final String name;
     private final String path;
-
-    public Process(String name, String path) {
-        this.name = name;
-        this.path = path;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof Process process)) {
-            return false;
-        }
-
-        return Objects.equals(name, process.name) && Objects.equals(path, process.path);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, path);
-    }
 
 }
--- collector/src/test/java/dev/zalaya/collector/domain/model/ProcessTest.java
+++ collector/src/test/java/dev/zalaya/collector/domain/model/ProcessTest.java
@@ -41,30 +41,4 @@ class ProcessTest {
         assertEquals(path, process.getPath());
     }
 
-    @Test
-    void equals_shouldReturnTrueWhenComparingTwoEqualInstances() {
-        // Arrange
-        String name = "process";
-        String path = "path/to/process";
-        Process process1 = new Process(name, path);
-        Process process2 = new Process(name, path);
-
-        // Assert
-        assertEquals(process1, process2);
-    }
-
-    @Test
-    void equals_shouldReturnFalseWhenComparingTwoDifferentInstances() {
-        // Arrange
-        String name1 = "process1";
-        String path1 = "path/to/process1";
-        String name2 = "process2";
-        String path2 = "path/to/process2";
-        Process process1 = new Process(name1, path1);
-        Process process2 = new Process(name2, path2);
-
-        // Assert
-        assertNotEquals(process1, process2);
-    }
-
 }
