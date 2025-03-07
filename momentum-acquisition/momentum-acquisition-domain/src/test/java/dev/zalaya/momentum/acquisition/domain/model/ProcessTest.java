f7d07f9ebbc58533c5d55bebd69a5bb5e8b6901e
Zalaya <enol.corzal@gmail.com>
2025-03-08 00:43:57 +0100

    refactor: remove unused tests that were not necessary

--- momentum-acquisition/momentum-acquisition-domain/src/test/java/dev/zalaya/momentum/acquisition/domain/model/ProcessTest.java
+++ momentum-acquisition/momentum-acquisition-domain/src/test/java/dev/zalaya/momentum/acquisition/domain/model/ProcessTest.java
@@ -2,72 +2,27 @@ package dev.zalaya.momentum.acquisition.domain.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProcessTest {
 
     @Test
-    void shouldInstantiateWithValidFields() {
-        // Act
-        Process process = new Process(1234, "process.exe", "path/to/process", 12.34, 43.21, 5);
-
-        // Assert
-        assertInstanceOf(Process.class, process);
-    }
-
-    @Test
-    void shouldRetrieveFieldValuesUsingGetters() {
+    void givenTwoInstancesWithEqualNamesOrPaths_whenComparing_thenReturnTrue() {
         // Arrange
-        Process process = new Process(1234, "process.exe", "path/to/process", 12.34, 43.21, 5);
-
-        // Assert
-        assertEquals(1234, process.getPid());
-        assertEquals("process.exe", process.getName());
-        assertEquals("path/to/process", process.getPath());
-        assertEquals(12.34, process.getCpu());
-        assertEquals(43.21, process.getMemory());
-        assertEquals(5, process.getThreads());
-    }
-
-    @Test
-    void shouldEnsureFieldsAreFinal() {
-        // Arrange
-        Field[] fields = Process.class.getDeclaredFields();
-
-        // Assert
-        for (Field field : fields) {
-            assertTrue(Modifier.isFinal(field.getModifiers()));
-        }
-    }
-
-    @Test
-    void shouldEnsureFieldsDoNotHaveSetters() {
-        // Arrange
-        Method[] methods = Process.class.getDeclaredMethods();
-
-        // Assert
-        for (Method method : methods) {
-            assertFalse(method.getName().startsWith("set"));
-        }
-    }
-
-    @Test
-    void shouldReturnTrueWhenComparingTwoEqualInstances() {
         Process process1 = new Process(1234, "process.exe", "path/to/process", 12.34, 43.21, 5);
         Process process2 = new Process(4321, "process.exe", "path/to/process", 43.21, 12.34, 10);
 
+        // Act & Assert
         assertEquals(process1, process2);
     }
 
     @Test
-    void shouldReturnFalseWhenComparingTwoDifferentInstances() {
+    void givenTwoInstancesWithDifferentNamesOrPaths_whenComparing_thenReturnFalse() {
+        // Arrange
         Process process1 = new Process(1234, "process1.exe", "path/to/process1", 12.34, 43.21, 5);
         Process process2 = new Process(4321, "process2.exe", "path/to/process2", 43.21, 12.34, 10);
 
+        // Act & Assert
         assertNotEquals(process1, process2);
     }
 
