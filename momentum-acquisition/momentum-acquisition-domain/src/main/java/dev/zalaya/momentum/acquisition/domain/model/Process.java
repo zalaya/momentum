18f2ba026d8344b1a1b42642ac8cb471b0ccc876
Zalaya <enol.corzal@gmail.com>
2025-03-09 02:26:19 +0100

    feat: add some more tests for the domain process model

--- momentum-acquisition/momentum-acquisition-domain/src/main/java/dev/zalaya/momentum/acquisition/domain/model/Process.java
+++ momentum-acquisition/momentum-acquisition-domain/src/main/java/dev/zalaya/momentum/acquisition/domain/model/Process.java
@@ -1,10 +1,8 @@
 package dev.zalaya.momentum.acquisition.domain.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value
-@EqualsAndHashCode(of = {"name", "path"})
 public class Process {
 
     Integer pid;
--- momentum-acquisition/momentum-acquisition-domain/src/test/java/dev/zalaya/momentum/acquisition/domain/model/ProcessTest.java
+++ momentum-acquisition/momentum-acquisition-domain/src/test/java/dev/zalaya/momentum/acquisition/domain/model/ProcessTest.java
@@ -2,28 +2,82 @@ package dev.zalaya.momentum.acquisition.domain.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProcessTest {
 
     @Test
-    void givenProcessesWithSameNameAndPath_whenCompared_thenTheyAreEqual() {
+    void givenProcessClass_whenCheckingModifiers_thenClassIsFinal() {
         // Given
-        Process process1 = new Process(1234, "process.exe", "path/to/process", 12.34, 43.21, 5);
-        Process process2 = new Process(4321, "process.exe", "path/to/process", 43.21, 12.34, 10);
+        int modifiers = Process.class.getModifiers();
 
         // When & Then
-        assertEquals(process1, process2);
+        assertTrue(Modifier.isFinal(modifiers));
     }
 
     @Test
-    void givenProcessesWithDifferentNameOrPath_whenCompared_thenTheyAreNotEqual() {
+    void givenProcessMethods_whenCheckingPublicMethods_thenNoPublicSettersExist() {
+        // Given
+        Method[] methods = Process.class.getMethods();
+
+        // When & Then
+        for (Method method : methods) {
+            if (method.getName().startsWith("set")) {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    void givenProcessFields_whenCheckingModifiers_thenFieldsArePrivate() {
+        // Given
+        Field[] fields = Process.class.getDeclaredFields();
+
+        // When & Then
+        for (Field field : fields) {
+            if (!Modifier.isPrivate(field.getModifiers())) {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    void givenProcessFields_whenCheckingModifiers_thenFieldsAreFinal() {
+        // Given
+        Field[] fields = Process.class.getDeclaredFields();
+
+        // When & Then
+        for (Field field : fields) {
+            if (!Modifier.isFinal(field.getModifiers())) {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    void givenIdenticalProcesses_whenCompared_thenTheyAreEqual() {
+        // Given
+        Process process1 = new Process(1234, "process.exe", "path/to/process", 12.34, 43.21, 5);
+        Process process2 = new Process(1234, "process.exe", "path/to/process", 12.34, 43.21, 5);
+
+        // When & Then
+        assertEquals(process1, process2);
+        assertEquals(process1.hashCode(), process2.hashCode());
+    }
+
+    @Test
+    void givenDifferentProcesses_whenCompared_thenTheyAreNotEqual() {
         // Given
         Process process1 = new Process(1234, "process1.exe", "path/to/process1", 12.34, 43.21, 5);
         Process process2 = new Process(4321, "process2.exe", "path/to/process2", 43.21, 12.34, 10);
 
         // When & Then
         assertNotEquals(process1, process2);
+        assertNotEquals(process1.hashCode(), process2.hashCode());
     }
 
 }
